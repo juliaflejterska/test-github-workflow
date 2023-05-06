4e872bba507e4bf085e77edd84fed47b19ea3837
juliaflejterska <flejterskajulia@gmail.com>
2023-05-06 15:26:22 +0200

    task1 fix

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""
+Module that contains a Flask application.
+
+This module defines the Flask app that serves the website.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +11,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Index page of the website.
+
+    This function returns a string containing an HTML response.
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 
